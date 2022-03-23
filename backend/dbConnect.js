06225436c52dcb0aeac93aee3b4d3412701e796a
kmsdoit <klmmms882912@naver.com>
2022-03-23 19:26:27 +0900

    mysql 연결

---
+++ backend/dbConnect.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const connectionPool = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+connectionPool.connect((err) => {
+  if (err) console.log(err);
+  else console.log("DB Connect Success");
+});
+
+connectionPool.end();
