16056b4aeeffc5880ba8199af81a9ee20dff5419
kmsdoit <klmmms882912@naver.com>
2022-03-24 20:32:45 +0900

    메일 인증

--- backend/app/service/user.ts
+++ backend/app/service/user.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from "express";
-import { join } from "path";
-import { hasUncaughtExceptionCaptureCallback } from "process";
 
 const connectionPool = require("../config/dbConnect");
-const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const algorithm = 'aes-256-cbc';
-const key = crypto.scryptSync('wolfootjaIsSpecial','specialSalt', 32); 
-const iv = crypto.randomBytes(16);
-const cipher = crypto.createCipheriv(algorithm, key, iv);
-const deciper = crypto.createDecipheriv(algorithm, key, iv);
-
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 const table = "user";
 
+const generateRandom = function (min:any, max:any) {
+  const ranNum = Math.floor(Math.random()*(max-min+1)) + min;
+  return ranNum;
+}
+
 module.exports = {
-  allUserApi: async (req:Request,res: Response) => {
+
+  allUserApi: async (res: Response) => {
     const sql = `select * from ${table}`;
+
     connectionPool.query(sql, (err: any, rows: any, fields: any) => {
       if (err) {
         res.status(400).send({ msg: "error", content: err });
@@ -30,53 +30,86 @@ module.exports = {
     const sql = `insert into ${table} set ?`;
     const body = req.body;
 
-    let inputPassword = body.user_password;
-    cipher.update(inputPassword,'utf8','base64');
-    let result = cipher.final('base6');
+    let currentPassword = body.user_password;
+    const encryptedPW = bcrypt.hashSync(currentPassword, 10) //아이디 암호화
+
 
     const param = {
       user_id : body.user_id,
-      user_password:result,
+      user_password:encryptedPW,
       user_name:body.user_name,
       user_email:body.user_email
     }
 
     connectionPool.query(sql, param, (err: any, rows: any, fields: any) => {
       if (err) {
+        
         res.status(400).send({ msg: "error", content: err });
       } else {
-        res.cookie('password',param);
-        res.status(200).send({ msg: "success" , param :param});
+        res.status(200).send({ msg: "success" , param:param});
       }
     });
   },
 
+  //login API
   loginApi: async (req: Request, res: Response) => {
       const sql = `select * from ${table} where user_id = ? and user_password = ?`;
       const param = req.body.param;
       const jwtTokenKey = process.env.ACCESS_TOKEN_SECRET;
-      
+      let currentPassword = param[1];
+      const encryptedPW = bcrypt.hashSync(currentPassword, 10) // 아이디 암호화
+      param[1] = encryptedPW; //복호화
 
       connectionPool.query(sql, param, (err: any, rows: any, fields: any) => {
         if (err) {
           res.status(404).send({ msg: "error", content: err });
         } else {
-          rows.map((loginData:any) => {
-            if (param[0] === loginData.user_id && param[1] === loginData.user_password) {
-              const token = jwt.sign({user_id:loginData.user_id},jwtTokenKey,{
-                expiresIn : '1h'
-              });
-
-              res.cookie('token',token);
-              res.status(200).json({
-                result: 'ok',
-                token
-              });
-            } else {
-              res.status(400).json({ error: 'invalid user' });
-            } 
-        });
-      }
+          const token = jwt.sign({
+            user_id: param[0]
+            },jwtTokenKey, {
+            expiresIn: '1h'
+            });
+            res.cookie('token',token);
+            res.status(200).json({
+                  result: 'ok',
+                  token
+            });
+        }
     })
   },
+
+   //이메일 보내는 API 
+   mailSender : async (req:Request,res:Response) => {
+    const email = req.body.user_email;
+    const number = generateRandom(111111,999999); // 난수 발생
+
+    let transporter = await nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS,
+      },
+    });
+  
+    // send mail with defined transport object
+    const info =  await transporter.sendMail({
+      from: `"WDMA Team" <${process.env.NODEMAILER_USER}>`,
+      to: `${email}`,
+      subject: 'WDMA Auth Number',
+      text: `${number}`,
+      html: `<b>${number}</b>`,
+    });
+
+    await transporter.sendMail(info, function (error:any, info:any) {
+        if (error) {
+            console.log(error);
+        }
+        console.log("Finish sending email : " + info);
+        res.status(200).send({msg : "success"});
+        transporter.close()
+    });
+  }
 }
